20bfc0d2fe1f561d8589a647937263f87867e554
Tharun <tharun.c@crediwatch.com>
2024-10-08 16:55:45 +0530

    uploads

--- models/Firm.js
+++ models/Firm.js
@@ -2,40 +2,38 @@ const mongoose = require("mongoose");
 
 // Firm Schema
 const firmSchema = new mongoose.Schema({
-    firName: { type: String, required: true },
-    area:{type:String,required:true},
-    offers:{type:String,required:true},
-    category: [
-      {
-        veg: { type: Boolean, required: true },
-        nonveg: { type: Boolean, required: true },
-      },
-    ],
-    region: [
-      {
-        northIndian: { type: Boolean, required: true },
-        southIndian: { type: Boolean, required: true },
-        chinese: { type: Boolean, required: true },
-        japanese: { type: Boolean, required: true },
-      },
-    ],
-    product:[
-      {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "product" // Referencing the "product" model correctly
-      }
-  ],
-  Vendor:[
+  firName: { type: String, required: true },
+  area: { type: String, required: true },
+  category: [
     {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Vendor" // Referencing the "product" model correctly
-    }
-],
-  
-  });
-  
+      veg: { type: Boolean, required: true },
+      nonveg: { type: Boolean, required: true },
+    },
+  ],
+  region: [
+    {
+      northIndian: { type: Boolean, required: true },
+      southIndian: { type: Boolean, required: true },
+      chinese: { type: Boolean, required: true },
+      japanese: { type: Boolean, required: true },
+    },
+  ],
+  product: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "product", // Referencing the "product" model correctly
+    },
+  ],
+  Vendor: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Vendor", // Referencing the "Vendor" model correctly
+    },
+  ],
+  image: { type: String, required: false } // Added image field to store the image URL or path
+});
 
 // Create the Firm model
 const Firm = mongoose.model("Firm", firmSchema);
 
-module.exports = Firm
+module.exports = Firm;
